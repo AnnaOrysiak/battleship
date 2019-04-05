0dc2ca860a4ac37ef4075b5b3d286f5563d99763
AnnaOrysiak <walachan88@gmail.com>
2019-04-05 10:45:02 +0200

    Updates

---
+++ precache-manifest.67f47045ef3e98a5a9da4ec2ccc2c86c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "23ffbe1947620286608bf860bd613862",
+    "url": "/battleship/static/media/miss.23ffbe19.mp3"
+  },
+  {
+    "revision": "5840025edb204ccffb1e57a88201fcd1",
+    "url": "/battleship/static/media/hit.5840025e.mp3"
+  },
+  {
+    "revision": "29f0b3366d34fd04d9fdc80b3ccad31b",
+    "url": "/battleship/static/media/hit.29f0b336.png"
+  },
+  {
+    "revision": "0cc3c9515a7d6218206b54d3715fa664",
+    "url": "/battleship/static/media/gameover.0cc3c951.mp3"
+  },
+  {
+    "revision": "71f5fd5526bd984b7aed",
+    "url": "/battleship/static/js/runtime~main.71f5fd55.js"
+  },
+  {
+    "revision": "8a62a754097d6f2d7fdf",
+    "url": "/battleship/static/js/main.8a62a754.chunk.js"
+  },
+  {
+    "revision": "e8d6289e9763e551c605",
+    "url": "/battleship/static/js/1.e8d6289e.chunk.js"
+  },
+  {
+    "revision": "8a62a754097d6f2d7fdf",
+    "url": "/battleship/static/css/main.61d87649.chunk.css"
+  },
+  {
+    "revision": "3c0190bc5d8db7014c8bfdc5097582c3",
+    "url": "/battleship/index.html"
+  }
+];
